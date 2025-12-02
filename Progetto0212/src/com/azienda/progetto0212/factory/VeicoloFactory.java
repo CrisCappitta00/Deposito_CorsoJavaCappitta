540dc10bec3a957995831777f43f1c3357a6f564
Cristina Cappitta <cristinacappitta@gmail.com>
2025-12-02 17:51:24 +0100

    Nuova correzione

--- Progetto0212/src/com/azienda/progetto0212/factory/VeicoloFactory.java
+++ Progetto0212/src/com/azienda/progetto0212/factory/VeicoloFactory.java
@@ -6,6 +6,7 @@ import com.azienda.progetto0212.customException.VeicoloNonValidoException;
 import com.azienda.progetto0212.model.Aereo;
 import com.azienda.progetto0212.model.Treno;
 import com.azienda.progetto0212.model.Veicolo;
+import com.azienda.progetto0212.utility.Controller;
 import com.azienda.progetto0212.utility.Validatore;
 
 public class VeicoloFactory {
@@ -17,15 +18,14 @@ public class VeicoloFactory {
 		System.out.println("Indicare il tipo di modello");
 		String modello = sc.nextLine();
 		System.out.println("Indicare la velocità media");
-		Float velocita = Float.parseFloat(sc.nextLine());
+		float velocita = Controller.leggiFloat(sc, "Indicare la velocità media:");
 		//Creo l’oggetto specifico in base al tipo
 		if (tipoVeicolo.equalsIgnoreCase("AEREO")) {
-			System.out.println("L'aereo è di linea? (true/false):");
-			boolean diLinea = Boolean.parseBoolean(sc.nextLine()); // meglio usare Controller.leggiBoolean()
+
+			boolean diLinea = Controller.leggiBoolean(sc, "L'aereo è di linea?");
 			return new Aereo(modello, velocita, diLinea);
 		} else { // tipo TRENO
-			System.out.println("Il treno è regionale? (true/false):");
-			boolean regionale = Boolean.parseBoolean(sc.nextLine());
+			boolean regionale = Controller.leggiBoolean(sc, "Il treno è regionale?");
 			return new Treno(modello, velocita, regionale);
 		}
 	}
